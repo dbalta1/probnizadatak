c7378e5ca2752fddc8997a7dec730312ac2f99fa
dbalta1 <dbalta1@etf.unsa.ba>
2021-10-25 11:25:30 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("probni zadatak");
+        System.out.println("dodajem i ovo");
 	// write your code here
     }
 }
